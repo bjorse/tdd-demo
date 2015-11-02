1d72cc00f903876ca01b3aeaedc22a754326b1af
Björn Johansson <bjn@learningwell.se>
2015-11-02 13:05:18 +0100

    Added default instance in constructor

--- TDD.Demo/TDD.Demo.Presentation/Shipments/Loaders/ChangedOrderItemResult.cs
+++ TDD.Demo/TDD.Demo.Presentation/Shipments/Loaders/ChangedOrderItemResult.cs
@@ -4,6 +4,11 @@ namespace TDD.Demo.Presentation.Shipments.Loaders
 {
     public class ChangedOrderItemResult
     {
+        public ChangedOrderItemResult()
+        {
+            Item = new ItemModel();
+        }
+
         public ItemModel Item { get; set; }
 
         public int? PreviousQuantity { get; set; }
